ddfd3b643c7742d285399e632ac6b8bea9cef9e8
crea <crea@student.42roma.it>
2024-02-06 15:59:52 +0100

    modified gil all

--- Utils&Scripts/git-all.sh
+++ Utils&Scripts/git-all.sh
@@ -3,20 +3,30 @@
 # Esegue git status per ottenere il resoconto prima del commit
 git status
 
-# Chiede all'utente di inserire il messaggio del commit
-echo "Insert commit message:"
-read COMMIT_MESSAGE
+# Verifica se ci sono modifiche non staged o non committate
+if ! git diff-index --quiet HEAD --; then
+    # Chiede all'utente di inserire il messaggio del commit se ci sono modifiche
+    echo "Inserisci il messaggio del commit:"
+    read COMMIT_MESSAGE
 
-# Esegue git add .
-git add .
+    # Esegue git add per aggiungere tutte le modifiche
+    git add .
 
-# Esegue git status per ottenere il resoconto prima del commit
-git status
+    # Mostra lo stato attuale dopo l'aggiunta
+    git status
 
-# Esegue git commit
-git commit -m "$COMMIT_MESSAGE"
+    # Esegue git commit
+    git commit -m "$COMMIT_MESSAGE"
+else
+    echo "Nessuna modifica da committare."
+fi
 
-# Esegue git push
-git push
-
-echo "Mission complete."
+# Controlla se c'è qualcosa da pushare
+NEEDS_PUSH=$(git cherry -v)
+if [ -z "$NEEDS_PUSH" ]; then
+    echo "Tutto è già aggiornato. Niente da pushare."
+else
+    # Esegue git push
+    git push
+    echo "Mission complete."
+fi
